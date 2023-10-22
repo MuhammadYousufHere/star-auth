36154631fe1012735748b9ab9039e270f334d677
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-22 17:47:34 +0500

    feat: prisma init

---
+++ prisma/migrations/20231022123651_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isVerfied" BOOLEAN DEFAULT false,
+    "profile" TEXT,
+    "bio" TEXT DEFAULT '',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
