6892828b9e6276c5a52aaf4d253ccc0503ec7577
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-22 19:48:11 +0500

    fix: dynnamic route

--- app/api/user/[name]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server'
-
-async function GET(
-  req: NextRequest,
-  { params: { name } }: { params: { name: string } }
-) {
-  return new Response(name)
-}
-export { GET }
