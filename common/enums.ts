e733f58e7918ecfa56fc14be72016de5a031eda3
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-10-22 18:01:29 +0500

    feat: custom colors

---
+++ common/enums.ts
@@ -0,0 +1,11 @@
+export enum Color {
+  BgDark = '#1B2627',
+  BgCard = '#202c2d',
+  BgMenu = '#2c3e3f',
+  BgInput = '#425455',
+  TextLight = '#e5fafa',
+  TextGray = '#b5c2c2',
+  TextDark = '#1b2627',
+  Primary = '#b2f35f',
+  Ripple = '#B2F35F55',
+}
